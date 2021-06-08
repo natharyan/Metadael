2cc875e9c3e725e3955454de84f662137b1ca026
Aryan Nath <74306760+natharyan@users.noreply.github.com>
2021-06-09 02:07:00 +0530

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,31 @@
+function changeImage(counter) {
+    var images = [
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+      '<i class="fas fa-blind"></i>',
+    ];
+
+    $(".loader .image1").html("" + images[counter] + "");
+  }
+
+  function loading() {
+    var num = 0;
+
+    for (i = 0; i <= 100; i++) {
+      setTimeout(function () {
+        $('.loader span').html(num + '%');
+
+        if (num == 100) {
+          loading();
+        }
+        num++;
+      }, i * 120);
+    };
+
+  }
